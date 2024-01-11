fa31ebda3342fafc3f472fa73c428e6ba1c295a5
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-01-11 18:15:29 -0300

    Arquivos da primeira aula de JavaScript

---
+++ primeiraAulaJS/script.js
@@ -0,0 +1,34 @@
+        // var varString = "Amarela"
+        // var varNumber = 0
+        // var primeira_variavel = 5
+        // console.log(primeira_variavel)
+        // primeira_variavel += 5
+        // console.log(primeira_variavel + 5)
+
+        // firstFunction(Number(prompt("Qual é a sua idade?")))
+        // firstFunction(Number(prompt("Qual é a sua idade?")))
+        // firstFunction(Number(prompt("Qual é a sua idade?")))
+        // function firstFunction(currentAge){           
+        //     // let currentAge = Number(prompt("Qual é a sua idade?"))
+        //     let years = 10
+        //     let futureAge = currentAge + years
+        //     console.log(`Sua idade atual é ${currentAge} anos, daqui a ${years} anos sua idade será ${currentAge + years}`)
+        //     if (futureAge >= 18) {
+        //         console.log("Você já poderá ser preso!")
+        //     }else{
+        //         console.log("Você ainda será menor de idade")
+        //     }
+        // }
+
+        // twoConsoles()
+        // function twoConsoles() {
+        //     console.log("console 1");
+        //     console.log("console 2");
+        // }
+
+        let firstArray = ['Azul', 'Vermelho', 'Verde']
+        console.log(firstArray);
+        console.log(firstArray[0]);
+        console.log(firstArray[1]);
+        console.log(firstArray[2]);
+        console.log(firstArray.length);

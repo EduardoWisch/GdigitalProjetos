b0507f8276282970d23cf8b67af9e93b6869a24e
EduardoWisch <127266926+EduardoWisch@users.noreply.github.com>
2024-02-15 21:12:07 -0300

    Add files via upload

---
+++ paises/script.js
@@ -0,0 +1,46 @@
+// let data = {
+//     nome: "Eduardo",
+//     idade: 18,
+//     isStudent: true,
+// }
+
+// fetch('https://webhook.site/0a5fcef8-67ef-499a-b450-b44ff44038c9', {
+//     method: "POST",
+//     body: JSON.stringify(data)
+// })
+
+function list() {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(json => json.json())
+        .then(countries => {
+            let row = document.querySelector("#row")
+            let countriesHtml = ""
+            for (let index = 0; index < countries.length; index++) {
+                const country = countries[index];
+                // console.log(country.translations.por.common)
+
+                //nome: country.translations.por.commo
+                //imagem: country.flags.png
+                //alt: country.flags.alt
+                //flag: country.flag
+                //regiao: country.region
+
+                countriesHtml += `
+                <div class="col-4 mt-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
+                    <div class="card-body">
+                      <h5 class="card-title">${country.translations.por.common}</h5>
+                      <p class="card-text">
+                        Região: ${country.region}
+                      </p>
+                    </div>
+                  </div>
+                `
+
+            }
+            row.innerHTML = countriesHtml
+        })
+}
+
+list()

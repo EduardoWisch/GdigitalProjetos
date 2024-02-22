151888af02ecb4adc452b176225af4c4c97ebfa4
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-02-22 06:04:50 -0300

    adicionando a função de busca

--- paises/script.js
+++ paises/script.js
@@ -9,39 +9,48 @@
 //     body: JSON.stringify(data)
 // })
 
-function list() {
-  fetch('https://restcountries.com/v3.1/all')
+function list(param) {
+  let url = 'https://restcountries.com/v3.1/all'
+  let get = document.getElementById('get').value
+  if (param && get) {
+    url = 'https://restcountries.com/v3.1/name/' + get
+  }
+  fetch(url)
     .then(json => json.json())
     .then(countries => {
+      console.log(countries)
+
       let row = document.querySelector("#row")
-      let countriesHtml = ""
+      let countriesHtmlFinal = ""
+
       for (let index = 0; index < countries.length; index++) {
         const country = countries[index];
-        // console.log(country.translations.por.common)
 
-        //nome: country.translations.por.commo
-        //imagem: country.flags.png
-        //alt: country.flags.alt
-        //flag: country.flag
-        //regiao: country.region
-
-        countriesHtml += `
-                <div class="col-4 mt-3">
-                <div class="card" style="width: 18rem;">
-                    <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
-                    <div class="card-body">
-                      <h5 class="card-title">${country.translations.por.common}</h5>
-                      <p class="card-text">
-                        Região: ${country.region}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                `
-
+        countriesHtml = `
+        <div class="col-4 mt-3">
+          <div class="card" style="width: 18rem;">
+            <img src="${country.flags.png}" class="card-img-top" alt="${country.flags.alt}">
+            <div class="card-body">
+              <h5 class="card-title">${country.translations.por.common}</h5>
+              <p class="card-text">
+              Região: ${country.region}
+              </p>
+            </div>
+          </div>
+        </div>
+        `
+        countriesHtmlFinal = countriesHtmlFinal + countriesHtml
       }
-      row.innerHTML = countriesHtml
+      row.innerHTML = countriesHtmlFinal
     })
 }
 
 list()
+
+// console.log(country.translations.por.common)
+
+//nome: country.translations.por.commo
+//imagem: country.flags.png
+//alt: country.flags.alt
+//flag: country.flag
+//regiao: country.region

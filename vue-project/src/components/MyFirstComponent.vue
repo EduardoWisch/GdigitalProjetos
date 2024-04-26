c729b86ff9c03ad6fcfc0c9c7010b6898bc6606f
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-04-25 21:02:51 -0300

    Ultima aula

--- vue-project/src/components/MyFirstComponent.vue
+++ vue-project/src/components/MyFirstComponent.vue
@@ -3,13 +3,27 @@
         <div class="modal">
             <span class="close-x" @click="close()">X</span>
             <h2>{{ modalTitle }}</h2>
+            <h1>{{ product.name }}</h1>
+            <label>Nome do produto</label>
+            <input type="text" v-model="productName">
+            <label>Valor do produto</label>
+            <input type="number" v-model="productValue">
+            <button @click="createProduct()" >Criar produto</button>
             <slot></slot>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
+        data() {
+            return {
+                productName: '',
+                productValue: '',
+
+            }
+        },
         props: {
             modalTitle: {
                 type: String,
@@ -18,18 +32,36 @@
             showModal: {
                 type: Boolean,
                 required: true,
+            },
+            product: {
+                type: Object,
+                required: true, 
             }
         },
         methods: {
             close() {
                 this.$emit('closeModal')
                 this.$emit('update:showModal', false)
+            },
+            createProduct(){
+                let data = {
+                    name: this.productName,
+                    amount: this.productValue
+                }
+                axios.post('product', data)
+                    .then((response) => {
+                            console.log(response)
+                    })
+                        .catch((error) => {
+                        console.log(error)
+                        })
             }
         },
     }
 </script>
 
 <style lang="scss" scoped>
+
 .backdrop-modal {
     position: fixed;
     top: 0;
@@ -44,8 +76,11 @@
 }
 
 .modal {
+    display: grid;
+    gap: 15px;
     width: 400px;
     height: 250px;
+    height: fit-content;
     padding: 30px;
     position: relative;
     background-color: white;
@@ -59,5 +94,25 @@
         cursor: pointer;
         font-size: 20px;
     }
+}
+    
+.modal > input {
+    border: 1px solid #cdcdcd;
+    border-radius: 10px;
+    padding: 12px;
+    font-size: 16px;
+}
+.modal > label {
+    font-size: 16px;
+}
+.modal > button {
+    transition: .3s;
+    height: 50px;
+    background-color: #cdcdcd;
+    border: none;
+    border-radius: 10px;
+}
+.modal > button:hover {
+    background-color: #bdbdbd;
 }
 </style>
--- vue-project/src/views/Teste.vue
+++ vue-project/src/views/Teste.vue
@@ -1,35 +1,93 @@
 <script>
     import Modal from '../components/MyFirstComponent.vue';
+    import axios from 'axios';
 
     export default {
         data() {
             return {
                 modalTitle: 'Meu título',
                 showModal: true,
+                selectedProduct: '',
+                
             }
         },
         methods: {
             modalIsClosed() {
                 console.log('O modal está fechado')
+            },
+            getProducts() {
+                axios.get('product')
+                    .then((response) => {
+                        this.products = response.data.data
+                        console.log(this.products)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+                console.log('A requisição acabou!')
+            })
+            },
+            deleteProduct() {
+                axios.delete(`/product/${this.selectedProduct.id}`)
+                .then((response) => {
+                    console.log(response)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
             }
         },
         components: {
             Modal,
         },
+        mounted () {
+            axios.defaults.baseURL = 'http://192.168.0.134:8000/api/'
+
+            this.getProducts()
+        }
     }
 </script>
 
 <template>
-    <Modal  v-model:showModal="showModal" :modalTitle="modalTitle" @closeModal="modalIsClosed"> 
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus voluptates placeat officia expedita aspernatur eaque, repellat veritatis laudantium distinctio impedit consectetur numquam ex odio adipisci doloribus possimus illo fugiat iusto.</p>
+    <Modal  v-model:showModal="showModal" :modalTitle="modalTitle" @closeModal="modalIsClosed"  :product="selectedProduct"> 
+
     </Modal>
-    <div class="about">
-      <h1>Teste</h1>
-      <button @click="showModal = true">Mostrar modal</button>
+    
+    <div 
+    v-for="product in products" 
+    :key="product.id" 
+    class="product-box" 
+    @click="selectedProduct = product" 
+    :class="selectedProduct.id === product.id ? 'selected-box' : ''">
+        <h3>{{product.name}}</h3>
+        <p>{{product.amount}}</p>
     </div>
-  </template>
-  
-  <style>
+
+<div class="about">
+  <h1>Teste</h1>
+  <button @click="showModal = true">Mostrar modal</button>
+  <button @click="deleteProduct()" >Deletar produto {{ selectedProduct.name }}</button>
+</div>
+</template>
+
+  <style scoped>
+
+.selected-box {
+    background-color: #fff2;;
+}
+
+  .product-box{
+    transition: 0.3s;
+    border: 1px solid white;
+    padding: 20px;
+    margin: 15px;
+    border-radius: 20px;
+  }
+  .product-box h3{
+    font-size: 21px ;
+  }
+
   @media (min-width: 1024px) {
     .about {
       min-height: 100vh;
